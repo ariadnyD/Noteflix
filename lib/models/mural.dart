65ffa71133e01db85f3b5fd9b52c30220f7b4db4
Ariadny Francisca Dantas Santos <ariadnyfran15@gmail.com>
2026-05-25 11:31:49 -0300

    feat: adicionando os flitros utilizando Strategy

---
+++ lib/models/mural.dart
@@ -0,0 +1,58 @@
+// lib/models/mural.dart
+import 'assistivel.dart';
+
+// 1. A Interface da Estratégia (O Contrato)
+abstract class OrdenacaoStrategy {
+  List<Assistivel> ordenar(List<Assistivel> itens);
+}
+
+// 2. Estratégia Concreta A: Ordem Alfabética
+class OrdenarPorTitulo implements OrdenacaoStrategy {
+  @override
+  List<Assistivel> ordenar(List<Assistivel> itens) {
+    // Criamos uma cópia da lista para não bagunçar a lista original em memória
+    var listaOrdenada = List<Assistivel>.from(itens);
+    listaOrdenada.sort((a, b) => a.titulo.compareTo(b.titulo));
+    return listaOrdenada;
+  }
+}
+
+// 3. Estratégia Concreta B: Melhores Avaliados (Maior nota)
+class OrdenarPorNota implements OrdenacaoStrategy {
+  @override
+  List<Assistivel> ordenar(List<Assistivel> itens) {
+    var listaOrdenada = List<Assistivel>.from(itens);
+    // Ordena da maior nota para a menor. Se não tiver nota, considera 0.
+    listaOrdenada.sort((a, b) {
+      double notaA = a.nota ?? 0.0;
+      double notaB = b.nota ?? 0.0;
+      return notaB.compareTo(notaA); 
+    });
+    return listaOrdenada;
+  }
+}
+
+// 4. O Contexto (O Mural que usa a estratégia)
+class Mural {
+  List<Assistivel> meusItens = [];
+  
+  // O Mural guarda a estratégia atual, mas não sabe qual é exatamente
+  OrdenacaoStrategy estrategiaAtual;
+
+  // Ao criar o mural, definimos uma estratégia padrão inicial
+  Mural(this.estrategiaAtual);
+
+  void adicionarItem(Assistivel item) {
+    meusItens.add(item);
+  }
+
+  // O botão da interface vai chamar esse método para trocar o filtro na hora!
+  void mudarOrdenacao(OrdenacaoStrategy novaEstrategia) {
+    estrategiaAtual = novaEstrategia;
+  }
+
+  // Quando a tela pedir os itens para desenhar, eles já saem ordenados
+  List<Assistivel> get itensOrdenados {
+    return estrategiaAtual.ordenar(meusItens);
+  }
+}
